2676264d591c71ff4300be2c0f3ba3fb5e37f3a8
striker <striker@strikeroff.(none)>
2011-05-04 21:49:26 +0400

    - rails

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+
+gem 'inherited_resources'
+gem 'pg'
+
+gem 'haml'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+gem 'sass'
+
+
+group :development,:test do
+  gem 'mongrel', '1.2.0.pre2'
+
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'autotest'
+  gem 'factory_girl_rails'
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirScanner::Application.config.secret_token = '84846e2cc587ca08050a740d725b0d94b52d4df3f459c6a45651298c8da41eb6f1fa059abeb9b2cd41b9db8e6d5b5ecadd66e92817581b4f6c70227092c3b401'
